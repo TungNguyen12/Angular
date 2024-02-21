4d908f9845de6b1e4e38476f78d88064ca183b08
TungNguyen12 <nvtung382@gmail.com>
2024-02-21 17:52:16 +0200

    adding shipping price and the route to the shipping component

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,23 +1,31 @@
-import { Injectable } from '@angular/core';
-import { Product } from './products';
+import { Injectable } from '@angular/core'
+import { Product } from './products'
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
-  items: Product[] = [];
+  items: Product[] = []
+
+  constructor(private http: HttpClient) {}
+
   addTocart(product: Product) {
-    this.items.push(product);
+    this.items.push(product)
   }
 
   getItems() {
-    return this.items;
+    return this.items
   }
 
   clearCart() {
-    this.items = [];
-    return this.items;
+    this.items = []
+    return this.items
   }
 
-  constructor() {}
+  getShippingPrices() {
+    return this.http.get<{ type: string; price: number }[]>(
+      '/assets/shipping.json'
+    )
+  }
 }
